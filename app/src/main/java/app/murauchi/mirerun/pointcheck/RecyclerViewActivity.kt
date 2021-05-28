0fdc922ee02e6f16e760bd0f1924d05124abf580
mirei <mirei.saxophone1010@gmail.com>
2021-05-28 09:58:35 +0900

    recyclerView完成

--- app/src/main/java/app/murauchi/mirerun/pointcheck/RecyclerViewActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/pointcheck/RecyclerViewActivity.kt
@@ -2,10 +2,38 @@ package app.murauchi.mirerun.pointcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.RealmResults
+import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
+
+    val realm: Realm = Realm.getDefaultInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        val pointList = readAll()
+
+        val adapter = RecyclerViewAdapter(this, pointList,true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        //adapter.addAll()
+    }
+
+    fun create(type: String, amount: String, limit: Int) {
+        realm.executeTransaction {
+            val record = it.createObject(Record::class.java)
+            record.type = type
+            record.amount = amount
+            record.limit = limit
+        }
+    }
+
+    fun readAll(): RealmResults<Record> {
+        return realm.where(Record::class.java).findAll().sort("limit")
     }
 }
