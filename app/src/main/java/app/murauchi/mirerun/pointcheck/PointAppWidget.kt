8f38bfa731c97a3719decf9b3eac8fa006248bab
mirei <mirei.saxophone1010@gmail.com>
2021-06-12 15:26:42 +0900

    ウィジェット？？？

--- app/src/main/java/app/murauchi/mirerun/pointcheck/PointAppWidget.kt
+++ app/src/main/java/app/murauchi/mirerun/pointcheck/PointAppWidget.kt
@@ -3,6 +3,7 @@ package app.murauchi.mirerun.pointcheck
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
@@ -12,6 +13,8 @@ import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+private const val PointCheckApp = "com.mirerun.murauchi.PointCheck"
+
 class PointAppWidget: AppWidgetProvider() {
     val realm: Realm = Realm.getDefaultInstance()
     override fun onUpdate(
@@ -19,54 +22,72 @@ class PointAppWidget: AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        //appWidgetManager.updateAppWidget(appWidgetId,views)
         for (appWidgetId in appWidgetIds) {
-            val pointData = readAll() //sort済をfindFirstで持ってくる->期限が１番近いもの
-            val widgetType = pointData?.type
-            val widgetAmount = pointData?.amount
-            val limitDate = pointData?.limitDate
-            //Date型からLocalDate型
-            val df = SimpleDateFormat("yyyy-MM-dd")
-            val target = LocalDate.parse(df.format(limitDate) , DateTimeFormatter.ofPattern("uuuu-MM-dd"))
-            val year = target.year.toString()
-            val month = (target.monthValue).toString()
-            val day = target.dayOfMonth.toString()
-            val widgetLimit = "${year}年${month}月${day}日"
-            val pendingIntent: PendingIntent = Intent(context, RecyclerViewActivity::class.java)
-                .let { intent ->
-                    PendingIntent.getActivity(context, 0, intent, 0)
-                }
-            val views: RemoteViews = RemoteViews(context.packageName, R.layout.app_widget_layout)
-                .apply {
-                //setTextViewText(R.id.titleText, widgetText),
-                setOnClickPendingIntent(R.id.buttonForList, pendingIntent)
-            }
-            views.setTextViewText(R.id.widgetTypeText, widgetType)
-            views.setTextViewText(R.id.widgetAmountText, widgetAmount)
-            views.setTextViewText(R.id.widgetLimitText, widgetLimit)
-
-            appWidgetManager.updateAppWidget(appWidgetId,views)
+            updateAppWidget(context, appWidgetManager, appWidgetId)
         }
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        /*appWidgetIds.forEach { appWidgetId ->
-            // Create an Intent to launch Activity
-            val pendingIntent: PendingIntent = Intent(context, RecyclerViewActivity::class.java)
+    }
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (context == null || intent == null) return
+
+        when (intent.action) {
+            PointCheckApp -> {
+                val pendingIntent: PendingIntent = Intent(context, RecyclerViewActivity::class.java)
+                        .let { intent ->
+                            PendingIntent.getActivity(context, 0, intent, 0)
+                        }
+                val views: RemoteViews = RemoteViews(context.packageName, R.layout.app_widget_layout)
+                views.setOnClickPendingIntent(R.id.buttonForList, pendingIntent)
+
+                // ウィジェットを更新
+                val myWidget = ComponentName(context, PointAppWidget::class.java)
+                val manager = AppWidgetManager.getInstance(context)
+                manager.updateAppWidget(myWidget, views)
+            }
+        }
+    }
+    //onUpdateのappWidgetId毎の処理はこちらで実装する
+    internal fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+    ) {
+
+        // データ読み出し
+        val pointData = readAll() //sort済をfindFirstで持ってくる->期限が１番近いもの
+        val widgetType = pointData?.type
+        val widgetAmount = pointData?.amount
+        val limitDate = pointData?.limitDate
+        //Date型からLocalDate型
+        val df = SimpleDateFormat("yyyy-MM-dd")
+        val target = LocalDate.parse(df.format(limitDate) , DateTimeFormatter.ofPattern("uuuu-MM-dd"))
+        val year = target.year.toString()
+        val month = (target.monthValue).toString()
+        val day = target.dayOfMonth.toString()
+        val widgetLimit = "${year}年${month}月${day}日"
+
+        // RemoteViews オブジェクトを作成
+        val pendingIntent: PendingIntent = Intent(context, RecyclerViewActivity::class.java)
                 .let { intent ->
                     PendingIntent.getActivity(context, 0, intent, 0)
                 }
+        val views: RemoteViews = RemoteViews(context.packageName, R.layout.app_widget_layout)
+        views.setTextViewText(R.id.widgetTypeText, widgetType)
+        views.setTextViewText(R.id.widgetAmountText, widgetAmount)
+        views.setTextViewText(R.id.widgetLimitText, widgetLimit)
+        views.setOnClickPendingIntent(R.id.buttonForList, pendingIntent)
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            val views: RemoteViews = RemoteViews(
-                context.packageName,
-                R.layout.app_widget_layout
-            ).apply {
-                //setTextViewText(R.id.titleText, widgetText),
-                setOnClickPendingIntent(R.id.buttonForList, pendingIntent)
-            }
-            //update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }*/
+        //Button押下通知用のPendingIntentを作成しに登録
+        val countIntent = Intent(context, PointAppWidget::class.java).apply { action = PointCheckApp }
+        val countPendingIntent =
+                PendingIntent.getBroadcast(context, 0, countIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        views.setOnClickPendingIntent(R.id.buttonForList, pendingIntent)
+
+        // ウィジェットを更新
+        appWidgetManager.updateAppWidget(appWidgetId, views)
     }
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
 
